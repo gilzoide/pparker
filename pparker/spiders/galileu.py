f0761cf8d4e9729944fac7632db7fea60ef4a47f
gilzoide <gilzoide@gmail.com>
2017-03-21 11:40:04 -0300

    FIX: extração de subtítulos da Galileu: verifica continuação

--- pparker/spiders/galileu.py
+++ pparker/spiders/galileu.py
@@ -62,26 +62,28 @@ class GalileuSpider(scrapy.Spider):
         # Regex de tags HTML: dá match em tag (abrindo ou fechando) e no
         # conteúdo antes da próxima
         tag_re = r"<([^>]+)>([^<>]*)"
-        def paragrafo_subtitulo(p):
-            return p.endswith('\t')
         # Pilha de tags, pra tracking da profundidade
-        pilha_tags = ['html']
+        pilha_tags = []
         # Lista que se tornará o resultado final =]
         resultado = []
-        # Booleano que marca quando estamos pegando um subtítulo
+        # Marca quando estamos pegando um subtítulo, e onde esse deve ficar
         eh_subtitulo = False
+        indice_subtitulo = 0
+        def checa_subtitulo():
+            return (pilha_tags[-1:] == ['p'] and resultado[-1].endswith('\t')) or \
+                    (pilha_tags[-2:] == ['p', 'em'] and resultado[-2].endswith('\t'))
 
-        all_tags = re.findall(tag_re, corpo)
-        for tag, txt in all_tags:
+        todas_tags = re.findall(tag_re, corpo)
+        for tag, txt in todas_tags:
             if tag == '/p' or tag == 'br /':
                 resultado.append('\n')
-            if tag == 'strong' and \
-                    ((pilha_tags[-1:] == ['p'] and paragrafo_subtitulo(resultado[-1])) or \
-                    (pilha_tags[-2:] == ['p', 'em'] and paragrafo_subtitulo(resultado[-2]))):
-                resultado.append('<subtitle>')
+            if tag == 'strong' and checa_subtitulo():
+                indice_subtitulo = len(resultado)
                 eh_subtitulo = True
             elif eh_subtitulo and tag == '/strong':
-                resultado.append('</subtitle>')
+                if txt == '':
+                    resultado.insert(indice_subtitulo, '<subtitle>')
+                    resultado.append('</subtitle>')
                 eh_subtitulo = False
             resultado.append(html.unescape(txt))
             # Empilha/desempilha tags
